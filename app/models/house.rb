5cead10fe94152693eb36c3e9a05701c4fc66392
Brenda Wihogora <brewihogora23@gmail.com>
2022-11-07 19:52:06 +0200

    Add validations

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,8 @@
 class House < ApplicationRecord
  belongs_to :user
   has_many :reservation
-
+ validates :name, presence: true
+ validates :photo, presence: true
+ validates :price, presence: true
+validates :location, presence: true
 end
