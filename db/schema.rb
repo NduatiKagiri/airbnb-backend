74e51e1ba2009b43cc4df56b1565a96c79baa895
Brenda Wihogora <brewihogora23@gmail.com>
2022-11-07 19:40:40 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_172159) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_173917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_172159) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "status"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_houses_on_user_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.date "date_in"
+    t.string "date_out"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "house_id", null: false
+    t.index ["house_id"], name: "index_reservations_on_house_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "houses", "users"
+  add_foreign_key "reservations", "houses"
 end
