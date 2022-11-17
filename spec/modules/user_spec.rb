bf625ee9880fc2360e9b62a69a58c259d03fa1b4
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-11-18 00:43:43 +0300

    Merge branch 'controllers' of https://github.com/NduatiKagiri/airbnb-backend into controllers

---
+++ spec/modules/user_spec.rb
@@ -0,0 +1,12 @@
+# require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   subject{User.new(name: 'Anyone', username: 'me', email: 'me@gmail.com', password_digest: '123456')}
+
+#   before {subject.save}
+
+#   it 'name should be present' do
+#     subject.name = nil
+#     expect(subject).to_not be_valid
+#   end
+# end
